85baff3ffe45c3c7a8023190b8f2b175b59241f2
Daniel Ogirev <marsik025@gmail.com>
2026-01-10 20:04:16 +0300

    chore: useContextStorageCollection - added default handlers

--- src/composables/useContextStorageCollection.ts
+++ src/composables/useContextStorageCollection.ts
@@ -2,8 +2,11 @@ import type { ContextStorageHandlerConstructor } from '../handlers'
 import { CollectionManager } from '../collection'
 import { provide } from 'vue'
 import { contextStorageCollectionInjectKey } from '../injectionSymbols'
+import { defaultHandlers } from '../constants'
 
-export function useContextStorageCollection(handlers: ContextStorageHandlerConstructor[]) {
+export function useContextStorageCollection(
+  handlers: ContextStorageHandlerConstructor[] = defaultHandlers,
+) {
   const collection = new CollectionManager(handlers)
 
   provide(contextStorageCollectionInjectKey, collection)
